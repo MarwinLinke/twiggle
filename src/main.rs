0c8fa89631c726f6740db6d774a1e2383d5672d6
Marwin Linke <marwin.linke@hu-berlin.de>
2025-11-04 00:10:38 +0100

    added paging for ambiguous prefixes

--- src/main.rs
+++ src/main.rs
@@ -9,51 +9,54 @@ use fetcher::get_dirs;
 use raw_mode::RawModeGuard;
 use screen::Screen;
 use std::{env, io};
-use visualize::{Mode, print_screen};
+use visualize::{Mode, View};
 
 fn main() -> io::Result<()> {
     let _raw_mode = RawModeGuard::new();
 
     let keybinds = "1234567890";
+    let screen = Screen::new();
 
-    let mut screen = Screen::new();
-    let mut mode = Mode::Normal;
+    let mut view: View = View::new(screen, keybinds.to_string());
+
     let mut ambiguous_char: Option<char> = None;
+    let mut current_page: Option<usize> = None;
+
+    let mut needs_redraw = true;
 
     loop {
-        disable_raw_mode()?;
-        screen.hide_cursor()?;
-        screen.move_up()?;
-
         let current_dir = std::env::current_dir()?;
         let (char_map, files) = get_dirs()?;
 
-        //eprintln!("{:?}", char_map);
+        if needs_redraw {
+            disable_raw_mode()?;
+            //eprintln!("{:?}", char_map);
+            view.prepare_screen()?;
+            view.print_screen(
+                &current_dir,
+                &char_map,
+                &files,
+                ambiguous_char,
+                current_page,
+            )?;
+            view.tidy_up_screen()?;
+            enable_raw_mode()?;
+        }
 
-        print_screen(
-            &mut screen,
-            &current_dir,
-            &char_map,
-            &files,
-            &mode,
-            ambiguous_char,
-            keybinds,
-        )?;
-        screen.clear_rest()?;
-        enable_raw_mode()?;
-        screen.show_cursor()?;
+        needs_redraw = false;
 
         if let Event::Key(e) = event::read()? {
             match e.code {
                 KeyCode::Char(c) => {
                     if c == '~' {
                         let home_dir = dirs::home_dir().expect("Could not find home directory");
+                        needs_redraw = true;
                         env::set_current_dir(&home_dir)?;
-                        mode = Mode::Normal;
+                        view.change_mode(Mode::Normal);
                         continue;
                     }
 
-                    match mode {
+                    match view.mode() {
                         Mode::Normal => {
                             let dict_entries = match char_map.get(&c) {
                                 Some(entry) => entry,
@@ -61,36 +64,53 @@ fn main() -> io::Result<()> {
                             };
 
                             if dict_entries.len() > 1 {
-                                mode = Mode::Sub;
+                                needs_redraw = true;
+                                view.change_mode(Mode::Sub);
+                                current_page = Some(0);
                                 ambiguous_char = Some(c);
                                 continue;
-                            } else {
-                                mode = Mode::Normal
                             }
 
+                            needs_redraw = true;
                             env::set_current_dir(dict_entries[0].file_name().unwrap())?;
                         }
                         Mode::Sub => {
+                            let dicts = char_map.get(&ambiguous_char.unwrap()).unwrap();
+
+                            let max_page = dicts.len() / 10;
+
+                            match c {
+                                'n' | 'f' => {
+                                    current_page = current_page.map(|p| (p + 1).min(max_page));
+                                    needs_redraw = true;
+                                }
+                                'b' => {
+                                    current_page = current_page.map(|p| p.saturating_sub(1));
+                                    needs_redraw = true;
+                                }
+                                _ => (),
+                            }
+
                             let index = match keybinds.find(c) {
-                                Some(i) => i,
+                                Some(i) => i + current_page.unwrap_or_default() * 10,
                                 None => continue,
                             };
 
-                            let dicts = char_map.get(&ambiguous_char.unwrap()).unwrap();
                             let dict = match dicts.get(index) {
                                 Some(dict) => dict,
                                 None => continue,
                             };
 
+                            needs_redraw = true;
                             env::set_current_dir(dict.file_name().unwrap())?;
-
-                            mode = Mode::Normal;
+                            view.change_mode(Mode::Normal);
                         }
                     }
                 }
                 KeyCode::Backspace => {
-                    if matches!(mode, Mode::Sub) {
-                        mode = Mode::Normal;
+                    needs_redraw = true;
+                    if matches!(view.mode(), Mode::Sub) {
+                        view.change_mode(Mode::Normal);
                     } else {
                         env::set_current_dir("..")?;
                     }
--- src/visualize.rs
+++ src/visualize.rs
@@ -6,115 +6,184 @@ use std::collections::BTreeMap;
 use std::path::Path;
 use std::path::PathBuf;
 
+#[derive(Debug, Clone, Copy)]
 pub enum Mode {
     Normal,
     Sub,
 }
 
-pub fn print_screen(
-    screen: &mut Screen,
-    current_dir: &Path,
-    char_map: &BTreeMap<char, Vec<PathBuf>>,
-    files: &[PathBuf],
-    mode: &Mode,
-    ambiguous_char: Option<char>,
-    keybinds: &str,
-) -> std::io::Result<()> {
-    let current_location = current_dir.to_string_lossy();
-
-    let header = format!(" {} ", current_location).black().on_blue().bold();
-
-    screen.write(header)?;
-    screen.empty_line()?;
-
-    match mode {
-        Mode::Normal => print_normal(screen, char_map),
-        Mode::Sub => print_sub(
-            screen,
-            char_map,
-            ambiguous_char.expect("No ambiguous char specified"),
-            keybinds,
-        ),
-    }?;
-
-    let file_str = files
-        .iter()
-        .map(|f| f.file_name().unwrap().to_string_lossy().to_string())
-        .collect::<Vec<String>>()
-        .join(" ");
-
-    let file_info = format!(
-        "{} {}",
-        " Files ".black().on_magenta().bold(),
-        file_str.magenta()
-    );
-
-    screen.write(file_info)?;
-    screen.empty_line()?;
-    screen.write("<Esc> to cancel | <Enter> to confirm")?;
-
-    Ok(())
+pub struct View {
+    screen: Screen,
+    keybinds: String,
+    current_mode: Mode,
 }
 
-fn print_normal(
-    screen: &mut Screen,
-    char_map: &BTreeMap<char, Vec<PathBuf>>,
-) -> std::io::Result<()> {
-    screen.write(" Directories ".black().on_dark_yellow().bold())?;
-
-    for dir in char_map {
-        let dir_str = dir
-            .1
-            .iter()
-            .map(|d| d.file_name().unwrap().to_string_lossy().to_string())
-            .collect::<Vec<String>>()
-            .join(" ");
-
-        let single = format!("[{}] {}", dir.0, format!("  {}", dir_str).dark_yellow());
-        let multiple = format!("[{}?] {}", dir.0, format!("󰉓  {}", dir_str).white());
-
-        if dir.1.len() > 1 {
-            screen.write(multiple)?;
-        } else {
-            screen.write(single)?;
+impl View {
+    pub fn new(screen: Screen, keybinds: String) -> Self {
+        View {
+            screen,
+            keybinds,
+            current_mode: Mode::Normal,
         }
     }
 
-    screen.empty_line()?;
-
-    Ok(())
-}
-
-fn print_sub(
-    screen: &mut Screen,
-    char_map: &BTreeMap<char, Vec<PathBuf>>,
-    ambiguous_char: char,
-    keybinds: &str,
-) -> std::io::Result<()> {
-    screen.write(
-        format!(" Select [{}] ", &ambiguous_char)
-            .black()
-            .on_dark_green()
-            .bold(),
-    )?;
-
-    //eprintln!("AMBIGUOUS CHAR: {}", ambiguous_char);
-
-    let dir_entries = char_map
-        .get(&ambiguous_char)
-        .expect("Char not found in map.");
-
-    for (i, dir) in dir_entries.iter().enumerate() {
-        let prefix = match keybinds.chars().nth(i) {
-            Some(c) => c.to_string(),
-            None => String::from("..."),
-        };
-
-        let dir_str = format!("  {}", dir.file_name().unwrap().to_string_lossy()).dark_green();
-        screen.write(format!("[{}] {}", prefix, dir_str))?;
+    pub fn change_mode(&mut self, mode: Mode) {
+        self.current_mode = mode;
     }
 
-    screen.empty_line()?;
+    pub fn mode(&self) -> Mode {
+        self.current_mode
+    }
 
-    Ok(())
+    pub fn prepare_screen(&mut self) -> std::io::Result<()> {
+        self.screen.hide_cursor()?;
+        self.screen.move_up()
+    }
+
+    pub fn tidy_up_screen(&mut self) -> std::io::Result<()> {
+        self.screen.clear_rest()?;
+        self.screen.show_cursor()
+    }
+
+    pub fn print_screen(
+        &mut self,
+        current_dir: &Path,
+        char_map: &BTreeMap<char, Vec<PathBuf>>,
+        files: &[PathBuf],
+        ambiguous_char: Option<char>,
+        current_page: Option<usize>,
+    ) -> std::io::Result<()> {
+        let current_location = current_dir.to_string_lossy();
+
+        let header = format!(" {} ", current_location).black().on_blue().bold();
+
+        self.screen.write(header)?;
+        self.screen.empty_line()?;
+
+        match self.current_mode {
+            Mode::Normal => self.print_normal(char_map),
+            Mode::Sub => self.print_sub(
+                char_map,
+                ambiguous_char.expect("No ambiguous char specified"),
+                current_page.expect("Current page is not set."),
+            ),
+        }?;
+
+        let file_str = files
+            .iter()
+            .map(|f| f.file_name().unwrap().to_string_lossy().to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let file_info = format!(
+            "{} {}",
+            " Files ".black().on_magenta().bold(),
+            file_str.magenta()
+        );
+
+        self.screen.write(file_info)?;
+        self.screen.empty_line()?;
+        self.screen.write("<Esc> to cancel | <Enter> to confirm")?;
+
+        Ok(())
+    }
+
+    fn print_normal(&mut self, char_map: &BTreeMap<char, Vec<PathBuf>>) -> std::io::Result<()> {
+        self.screen
+            .write(" Directories ".black().on_dark_yellow().bold())?;
+
+        for dir in char_map {
+            let dir_str = dir
+                .1
+                .iter()
+                .map(|d| d.file_name().unwrap().to_string_lossy().to_string())
+                .collect::<Vec<String>>()
+                .join(" ");
+
+            let single = format!("[{}] {}", dir.0, format!("  {}", dir_str).dark_yellow());
+            let multiple = format!("[{}?] {}", dir.0, format!("󰉓  {}", dir_str).white());
+
+            if dir.1.len() > 1 {
+                self.screen.write(multiple)?;
+            } else {
+                self.screen.write(single)?;
+            }
+        }
+
+        self.screen.empty_line()?;
+
+        Ok(())
+    }
+
+    fn print_sub(
+        &mut self,
+        char_map: &BTreeMap<char, Vec<PathBuf>>,
+        ambiguous_char: char,
+        current_page: usize,
+    ) -> std::io::Result<()> {
+        self.screen.write(
+            format!(" Select [{}] ", &ambiguous_char)
+                .black()
+                .on_dark_green()
+                .bold(),
+        )?;
+
+        //eprintln!("AMBIGUOUS CHAR: {}", ambiguous_char);
+
+        let dir_entries = char_map
+            .get(&ambiguous_char)
+            .expect("Char not found in map.");
+
+        let page_size = 10;
+        let start_idx = current_page * page_size;
+        let end_idx = (start_idx + page_size).min(dir_entries.len());
+
+        let current_slice: Vec<&PathBuf> =
+            dir_entries.iter().skip(start_idx).take(page_size).collect();
+
+        let other_dirs: Vec<&PathBuf> = dir_entries
+            .iter()
+            .take(start_idx) // before current page
+            .chain(dir_entries.iter().skip(end_idx)) // after current page
+            .collect();
+
+        for (i, dir) in current_slice.iter().enumerate() {
+            let prefix = match self.keybinds.chars().nth(i) {
+                Some(c) => c.to_string(),
+                None => String::from("..."),
+            };
+
+            let dir_str = format!("  {}", dir.file_name().unwrap().to_string_lossy()).dark_green();
+            self.screen.write(format!("[{}] {}", prefix, dir_str))?;
+        }
+
+        // WARNING: Might actually break with exactly 10 directories
+        let max_pages = dir_entries.len() / 10;
+        if max_pages > 0 {
+            let other_dirs_info = format!(
+                "Other Directories: {}",
+                other_dirs
+                    .iter()
+                    .map(|d| d.file_name().unwrap().to_string_lossy().to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            );
+
+            self.screen.write(other_dirs_info)?;
+
+            let page_info = format!(" Page [{}/{}] ", current_page + 1, max_pages + 1)
+                .black()
+                .on_dark_green()
+                .bold();
+
+            let navigation_info = "<f> page forward | <b> page backward";
+
+            self.screen
+                .write(format!("{} {}", page_info, navigation_info))?;
+        }
+
+        self.screen.empty_line()?;
+
+        Ok(())
+    }
 }
